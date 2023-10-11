e9aeedf44ceb65aeb576a6d0aa3276d568d0f9e5
Jose Santos <josesantos@microsoft.com>
2023-10-11 14:48:54 -0700

    Checking commit permission

--- intro/src/main/scala/_07_Class.scala
+++ intro/src/main/scala/_07_Class.scala
@@ -1,14 +1,16 @@
 class Person (var name: String, var email: String, var age: Int) {
-  def asString(): String = s"[Name: $name; Email: $email; Age: $age]"
+  override def toString(): String = s"[Name: $name; Email: $email; Age: $age]"
 }
 
 object _07_Class extends App{
   val person1 = new Person("Jose", "jose@email.com", 51)
-  println(person1.asString())
+  println(person1.toString())
   person1.name = "Leila"
   person1.email = "leila@email.com"
   person1.age = 40
-  println(person1.asString())
+  println(person1.toString())
+  val person2 = new Person("Artur", "artur@noemail.com", 20)
+  println(person2.toString())
 }
 
 
